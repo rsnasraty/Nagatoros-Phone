97a1d49a990b8bd83dd92a95299a3fcbe37ee891
Roxanne Nasraty <rsnasraty@gmail.com>
2021-06-30 21:22:55 -0500

    Made login form

---
+++ login.js
@@ -0,0 +1,17 @@
+<form action ="/login" method="post">
+<ul>
+<li>
+<label for="Username">Username:</label>
+<input type="text" id="Username" name="Username"></input>
+</li>
+
+<li>
+<label for="Password">Password:</label>
+<input type="password" id="Password" name="Password"></input>
+</li>
+<li>
+<label for="Email">Email:</label>
+<input type="email" id="Email" name="Email"></input> 
+</li>
+</ul>
+</form>
